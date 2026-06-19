d8d25ae3c4e74101f3b65d2e2f89abc3be8d50c3
Carlos Eduardo <parraaa0901@gmail.com>
2026-06-18 21:06:25 -0700

    fix: database.py

--- database.py
+++ database.py
@@ -2,11 +2,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv(sqlite:///./finance.db)
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./finance.db")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
-Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
