825bf13b9ccac8f0e31d9ee8f1918f3944bf86a4
Carlos Eduardo <parraaa0901@gmail.com>
2026-06-18 21:04:11 -0700

    feat: database.py
    database.py implemented

---
+++ database.py
@@ -0,0 +1,20 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = os.getenv(sqlite:///./finance.db)
+
+engine = create_engine(DATABASE_URL)
+
+Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+
+    finally:
+        db.close()
