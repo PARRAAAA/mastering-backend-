554351dbbab418c9b402f7d724f298d825a2f13c
Carlos Eduardo <parraaa0901@gmail.com>
2026-06-23 23:08:42 -0700

    feat: test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,15 @@
+import pytest
+from sqlalchemy.exc import IntegrityError
 from models import Account, Transaction
+
+
+def test_create_account(db_session):
+    account = Account(name="Checking")
+    db_session.add(account)
+    db_session.commit()
+
+    assert account.id is not None
+    assert account.balance == 0.0
 
 
 def test_transaction_belongs_to_account(db_session):
@@ -18,3 +29,15 @@ def test_transaction_belongs_to_account(db_session):
     assert transaction.account_id == account.id
     assert transaction.account == account
     assert account.transactions == [transaction]
+
+
+def test_transaction_requires_account_id(db_session):
+    transaction = Transaction(
+        amount=-20.0,
+        description="Coffee",
+        category="Food",
+    )
+    db_session.add(transaction)
+
+    with pytest.raises(IntegrityError):
+        db_session.commit()
